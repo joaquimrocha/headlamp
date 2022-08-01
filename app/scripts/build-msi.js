cb932a2cc683bd1bece060c54c338732c334aec9
Joaquim Rocha <joaquim.rocha@microsoft.com>
2022-08-01 17:37:43 +0100

    app: Use package.json info to build Windows MSI
    This patch uses the info we have already on package.json, official one
    as well as the electrno-builder's one, to build the MSI installer.
    
    This means we get an MSI file whose name is consistent with the ones
    we generate from electron-builder.

--- app/scripts/build-msi.js
+++ app/scripts/build-msi.js
@@ -2,23 +2,41 @@
 // Reference https://ourcodeworld.com/articles/read/927/how-to-create-a-msi-installer-in-windows-for-an-electron-framework-application
 const { MSICreator } = require('electron-wix-msi');
 const path = require('path');
+const info = require('../package.json');
 
 const APP_DIR = path.resolve(__dirname, '../dist/win-unpacked');
 const OUT_DIR = path.resolve(__dirname, '../dist');
 
-const msiCreator = new MSICreator({
+const nameOptions = {
+  productName: info.productName,
+  version: info.version,
+  os: 'win',
+  arch: 'x86',
+};
+
+// Generate the exe name from electron-builder's artifactName
+let exeName = info.build.artifactName.split('.')[0];
+Object.entries(nameOptions).forEach(([key, value]) => {
+  exeName = exeName.replace(`\${${key}}`, value);
+});
+
+const msiOptions = {
   appDirectory: APP_DIR,
   outputDirectory: OUT_DIR,
-  description: 'Easy-to-use and extensible Kubernetes web UI',
-  exe: process.env.npm_package_name,
-  name: process.env.npm_package_name,
-  manufacturer: 'Kinvolk',
+  description: info.description,
+  exe: exeName,
+  name: nameOptions.productName,
+  manufacturer: info.author.name,
   version: process.env.npm_package_version,
   appIconPath: path.resolve(__dirname, '../build/icons/icon.ico'),
   ui: {
     chooseDirectory: true,
   },
-});
+};
+
+console.info('Generating MSI with the following options:', msiOptions);
+
+const msiCreator = new MSICreator(msiOptions);
 
 msiCreator.create().then(function () {
   msiCreator.compile();
